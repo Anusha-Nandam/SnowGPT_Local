660024041bc060f264aa4f6f8718091156ff3339
Anusha-Nandam <77226044+Anusha-Nandam@users.noreply.github.com>
2023-09-27 16:25:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ snowflake_database = "SNOWGPT_DB"
 snowflake_schema = "STG"
 stage_name = "snowgpt_s3_stage"
 
+
 query = "SELECT DISTINCT GET_PRESIGNED_URL(@snowgpt_s3_stage, METADATA$FILENAME) FROM @snowgpt_s3_stage"
 
 api_key = "741d647c-da49-4048-9d88-3b56e9e8e7f3"
